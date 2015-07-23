ad276485949846d6f4ab4361bbdafb82baafde25
Danitza Vlahova <pevanzi@abv.bg>
2015-07-23 19:30:27 +0300

    gadgdgsfgfg

--- scripts/RenderEngines.js
+++ scripts/RenderEngines.js
@@ -8,10 +8,11 @@ var RenderEngines = (function() {
                 width: width,
                 height: height
             });
-
+           
             this.layer = new Kinetic.Layer();
             this.stage.add(this.layer);
 
+           
             this.canvas = document.getElementsByTagName('canvas')[0];
             this.canvas.width = width;
             this.canvas.height = height;
@@ -20,16 +21,51 @@ var RenderEngines = (function() {
             return this;
         }
 
+
+		 function drawBackground() {
+		 	var ctx = this.ctx;
+		 	//this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		 	
+			var img = new Image();
+
+			img.onload = function () {
+
+				var pattern = ctx.createPattern(img, 'repeat');
+				ctx.fillStyle = pattern;
+				ctx.fillRect(-20, 0, 680, 500);
+				this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+			};
+
+			img.src = 'media/grass.jpg';
+		
+
+
+
+		 }
         function renderState(worldObjects) {
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        	//this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            
             renderSnake.call(this, worldObjects.snake);
             renderWalls.call(this, worldObjects.walls);
             renderFood.call(this, worldObjects.food);
+            
+            drawBackground.call(this);
+            
             // this.layer.draw();
         }
 
         function renderSnake(snake) {
             var ctx = this.ctx;
+
+            var img = new Image();
+
+			img.onload = function () {
+				var pattern = ctx.createPattern(img, 'repeat');
+				ctx.fillStyle = pattern;
+				ctx.fillRect(0, 0, 640, 480);
+			};
+
+			img.src = 'pattern.jpg';
 
             for (var i = (snake.tail.length - 1); i >= 0; i -= 1) {
                 ctx.beginPath();
